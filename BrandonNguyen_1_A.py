6284954e730d7bc23cbe89cf9f78df2d1b1d8932
Brandon Nguyen <bnguyen732@gmail.com>
2024-08-12 14:48:55 -0700

    added a condition to check if the input is valid

--- BrandonNguyen_1_A.py
+++ BrandonNguyen_1_A.py
@@ -1,7 +1,7 @@
 # Ticket Sales Program
 # Name: Brandon Nguyen
 # Assignment: 1A
-# A application where a limited number of tickets are available for purchase.
+# A application where 20 tickets are available for purchase.
 # The user can request to buy a certain number of tickets between 1 and 4 until all tickets are sold out.
 # Also, keeps track of the remaining tickets and the total number of buyers.
 
@@ -9,34 +9,36 @@ def sell_tickets():
     total_tickets = 20
     total_buyers = 0
 
-    # Loop until all tickets are sold
+    # Loop until all tickets are sold.
     while total_tickets > 0:
 
-        # Prompt the user for the number of tickets they want to buy
-        tickets_bought = int(input("How many tickets would you like to buy? (1-4): "))
+        while True:
 
-        # Check the requested number of tickets
+            # Prompt the user for the number of tickets they want to buy.
+            user_input = input("How many tickets would you like to purchase? (1-4): ")
+
+            # check if the input is a number
+            if user_input.isdigit():
+                tickets_bought = int(user_input)
+                break
+            else:
+                print("Invalid input. Please enter a number.")
+
+        # Confirm the requested number of tickets.
         if 1 <= tickets_bought <= 4:
 
-            # Check if there are enough tickets left to fulfill the request
+            # Check if there are enough tickets to buy.
             if tickets_bought <= total_tickets:
                 total_tickets -= tickets_bought
                 total_buyers += 1
-
                 print(f"Tickets successfully purchased! Remaining tickets: {total_tickets}")
-
             else:
                 print(f"Sorry, only {total_tickets} tickets remaining.")
-
         else:
             print("You can only buy between 1 and 4 tickets.")
-
-        if total_tickets == 0:
-            break
 
     # Display the total number of buyers after all tickets are sold
     print(f"All tickets are sold out! Total buyers: {total_buyers}")
 
 
-# Start the ticket selling process
 sell_tickets()
