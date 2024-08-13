9957afb201057e6f7144f6ceeaeb346a96cc8e17
Brandon Nguyen <bnguyen732@gmail.com>
2024-08-13 10:08:33 -0700

    created a function that displays the total number of buyers

--- BrandonNguyen_1_A.py
+++ BrandonNguyen_1_A.py
@@ -1,40 +1,37 @@
-# An application that tracks the amount of buyers for a total of 20 tickets available for purchase.
+# An application where a limited number of tickets are available for purchase.
 # The user can request to buy a certain number of tickets between 1 and 4 until all tickets are sold out.
+# Also, keeps track of the remaining tickets and the total number of buyers.
 
-def sell_tickets():
+def ticket_tracker():
     total_tickets = 20
     total_buyers = 0
 
-    # Loop until all tickets are sold.
+    # Loop until all tickets are sold
     while total_tickets > 0:
 
-        while True:
+        # Prompt the user for the number of tickets they want to buy
+        tickets_bought = int(input("How many tickets would you like to buy? (1-4): "))
 
-            # Prompt the user for the number of tickets they want to buy.
-            user_input = input("How many tickets would you like to purchase? (1-4): ")
-
-            # check if the input is a number
-            if user_input.isdigit():
-                tickets_bought = int(user_input)
-                break
-            else:
-                print("Invalid input. Please enter a number.")
-
-        # Confirm the requested number of tickets.
+        # Check the requested number of tickets
         if 1 <= tickets_bought <= 4:
 
             # Check if there are enough tickets to buy.
             if tickets_bought <= total_tickets:
                 total_tickets -= tickets_bought
                 total_buyers += 1
-                print(f"Tickets successfully purchased! Remaining tickets: {total_tickets}")
+
+                print(f"Your transaction has been processed! Remaining tickets: {total_tickets}")
+
             else:
                 print(f"Error! Only {total_tickets} tickets remaining.")
         else:
-            print("You can only buy between 1 and 4 tickets.")
+            print(" Error! You are allowed to purchase between 1 and 4 tickets")
+
+        if total_tickets == 0:
+            break
 
     # Display the total number of buyers after all tickets are sold
     print(f"All tickets are sold out! Total buyers: {total_buyers}")
 
 
-sell_tickets()
+ticket_tracker()
